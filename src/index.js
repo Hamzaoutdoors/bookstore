e49485214147ff4bffeab7862f995e12239b35d6
Hamza <hamzaoutdoors@gmail.com>
2021-09-29 13:39:00 +0100

    Add Provider to add store to the app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './redux/cofigureStore';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
