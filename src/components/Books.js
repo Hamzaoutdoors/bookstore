8c88bb28027cdf5dc165e79c3269bf109e41576d
Hamza <hamzaoutdoors@gmail.com>
2021-09-30 18:03:48 +0100

    Retrieve data and render all books

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,46 +1,46 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import Book from './Book';
 import FormComponent from './Form';
-import { addBook, removeBook } from '../redux/books/books';
-import store from '../redux/cofigureStore';
+import { createBook, removeBook, getBook } from '../redux/books/books';
 
 const Books = () => {
   const dispatch = useDispatch();
-  const [booksData, setBooksData] = useState(store.getState().booksReducer);
+  const booksData = useSelector((state) => state.booksReducer);
+
+  useEffect(() => {
+    dispatch(getBook());
+  }, []);
 
   const submitBookToStore = (book) => {
     const newBook = {
-      id: uuidv4(), // generate unique ID
+      item_id: uuidv4(),
       title: book.title,
-      author: book.author,
+      category: book.category,
     };
     // dispatch an action and pass it the newBook object (your action's payload)
-    dispatch(addBook(newBook));
-    setBooksData((prevState) => [...prevState, newBook]);
+    dispatch(createBook(newBook));
   };
 
   const deleteBook = (book) => {
-    dispatch(removeBook(book));
-    const newBooks = booksData.filter((item) => item.id !== book.id);
-    setBooksData(newBooks);
+    dispatch(removeBook(book.item_id));
   };
 
+  const booksList = booksData.map((book) => (
+    <Book
+      title={book.title}
+      category={book.category}
+      key={booksData.indexOf(book)}
+      rmvBook={() => {
+        deleteBook(book);
+      }}
+    />
+  ));
   return (
 
     <div className="books">
-      { booksData.map((book) => (
-        <Book
-          title={book.title}
-          author={book.author}
-          key={book.id}
-          rmvBook={() => {
-            deleteBook(book);
-          }}
-        />
-      ))}
-
+      { booksList }
       <FormComponent submitBook={submitBookToStore} />
     </div>
   );
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,25 +1,67 @@
-import initialBooksData from './initialBooksData';
+import axios from 'axios';
 
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const GET_BOOKS = 'bookStore/books/GET_BOOKS';
+const baseURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/pu9kvo4W2aaAHOXLkIzL/books';
 
-export const addBook = (book) => ({
-  type: ADD_BOOK,
-  book,
-});
+const initialBooks = [];
 
-export const removeBook = (book) => ({
-  type: REMOVE_BOOK,
-  book,
-});
+export const createBook = (book) => async (dispatch) => {
+  const res = await axios.post(baseURL,
+    {
+      item_id: book.item_id,
+      title: book.title,
+      category: book.category,
+    });
+  const created = await res.data;
+  if (created === 'Created') {
+    dispatch({
+      type: ADD_BOOK,
+      book,
+    });
+  }
+};
 
-const reducer = (state = initialBooksData, action) => {
+export const removeBook = (bookId) => async (dispatch) => {
+  const res = await axios.delete(`${baseURL}/${bookId}`);
+  const deleted = await res.data;
+  if (deleted) {
+    dispatch({
+      type: REMOVE_BOOK,
+      bookId,
+    });
+  }
+};
+
+export const getBook = () => async (dispatch) => {
+  const res = await axios.get(baseURL);
+  const data = await res.data;
+  if (data) {
+    const objectArray = Object.entries(data);
+    const booksArray = [];
+    objectArray.forEach(([key, value]) => {
+      const item = { ...value, item_id: key };
+      const itemObj = Object.values(item);
+      booksArray.push(itemObj[0]);
+    });
+    dispatch({
+      type: GET_BOOKS,
+      booksArray,
+    });
+  }
+};
+
+const reducer = (state = initialBooks, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.book];
 
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.book.id);
+      return state.filter((book) => book.item_id !== action.bookId);
+
+    case GET_BOOKS:
+      return [...action.booksArray];
 
     default:
       return state;
